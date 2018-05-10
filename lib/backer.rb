ba18da8294ff590e71dad2da3dc2e52bba380c52
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-10 18:52:28 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,12 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name,
+
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project(title))
+    @backed_projects << Project.new(title)
   end
 end
